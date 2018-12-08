5528ea076a549885643df21e43d8661522eb1184
Maciek Wielgus <maciek.wielgus@gmail.com>
2018-12-07 22:02:59 -0500

    fix bugs

--- plot_coverage_uv.py
+++ plot_coverage_uv.py
@@ -106,7 +106,7 @@ line_color=[0,0,0,1], size_dots_primary=6*1.3, size_dots_redundant=6*1.,
             basefull = AZ2SMT[base[:2]]+'-'+AZ2SMT[base[3:]]
         #print(basefull,current_palette[basefull])
         if ('JCMT' in basefull) or (('APEX' in basefull) and ('ALMA' not in basefull)) :
-            colorLoc=current_palette[basefull]
+            colorLoc=np.asarray([current_palette[basefull]])
             plt.scatter(foo[foo.baseline==base].u,foo[foo.baseline==base].v,
                         s=size_dots_redundant,
                         c=colorLoc,label=basefull)
@@ -118,7 +118,7 @@ line_color=[0,0,0,1], size_dots_primary=6*1.3, size_dots_redundant=6*1.,
         else:    
             basefull = AZ2SMT[base[:2]]+'-'+AZ2SMT[base[3:]]
         if ('JCMT' not in basefull) and (('APEX' not in basefull) or ('ALMA' in basefull)) :
-            colorLoc=current_palette[basefull]
+            colorLoc=np.asarray([current_palette[basefull])
             plt.scatter(foo[foo.baseline==base].u,foo[foo.baseline==base].v,
                         s=size_dots_primary,
                         c=colorLoc,label=basefull)
